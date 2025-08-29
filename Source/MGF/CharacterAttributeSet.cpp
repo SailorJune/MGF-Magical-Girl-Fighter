75a342a766c301aebdb97347f9d00f3d5151a9aa
Juniper DeBaun <rosejune2203@gmail.com>
2025-08-29 12:01:39 -0500

    Fixed Repo
    I am dumb

---
+++ Source/MGF/CharacterAttributeSet.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterAttributeSet.h"
+
+UCharacterAttributeSet::UCharacterAttributeSet()
+{
+
+}
+
+void UCharacterAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+
+}
